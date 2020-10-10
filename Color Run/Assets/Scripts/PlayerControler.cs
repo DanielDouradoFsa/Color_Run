dc3af50f02f8bed1ada718795ebaf2dccd7c87ee
Roberto Maia <romaia.uefs@gmail.com>
2020-10-10 11:50:14 -0300

    Added Assets and Player Jump and Run

---
+++ Color Run/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed;
+    public float jumpHeight;
+    public float gravity;
+    private float jumpVelocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foward é o eixo Z para frente
+        Vector3 direction = Vector3.forward * speed;
+        if(controller.isGrounded){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                jumpVelocity = jumpHeight; // faz ele pular
+            }
+        }
+        else{
+            jumpVelocity -= gravity; //faz ele cair
+        }
+
+        direction.y= jumpVelocity; //pular
+        controller.Move(direction*Time.deltaTime); //mover
+    }
+}
