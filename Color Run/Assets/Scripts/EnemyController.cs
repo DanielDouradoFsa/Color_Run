24778ad7defca6286f1f4000ada40d2b03495a9b
DanielDouradoFsa <60206918+DanielDouradoFsa@users.noreply.github.com>
2020-10-22 22:36:51 -0300

    Mudança na movimentação do inimigo

--- Color Run/Assets/Scripts/EnemyController.cs
+++ Color Run/Assets/Scripts/EnemyController.cs
@@ -10,24 +10,36 @@ public class EnemyController : MonoBehaviour
     public float horizontalSpeed;
     public Animator anim;
 
+    public Transform playerPosition;
+
+
     void Start()
     {
         controller = GetComponent<CharacterController> ();
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController> ();
+       
     }
 
     void Update()
     {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        Debug.Log(playerPosition.position.x);
         speed = -playerController.speed;
         horizontalSpeed = playerController.horizontalSpeed;
         Vector3 direction = Vector3.back * speed;
 
-        if(Input.GetKey(KeyCode.RightArrow) && !playerController.isMovingR && transform.position.x <= 4f){
+        /*if(Input.GetKey(KeyCode.RightArrow) && !playerController.isMovingR && transform.position.x <= 4f){
             StartCoroutine(RightMove());//move para a direita 
+        }*/
+        if(playerPosition.position.x<-2.48 && playerPosition.position.x>-4.79){
+            controller.Move(Vector3.left * playerPosition.position.x);
         }
-        if(Input.GetKey(KeyCode.LeftArrow) && !playerController.isMovingL && transform.position.x >= -4f){
+        if(playerPosition.position.x>2.48 && playerPosition.position.x<4.56){
+            controller.Move(Vector3.right * playerPosition.position.x);
+        }
+        /*if(Input.GetKey(KeyCode.LeftArrow) && !playerController.isMovingL && transform.position.x >= -4f){
             StartCoroutine(LeftMove());//move para a esquerda
-        } 
+        }*/ 
         speed += speed*(Time.deltaTime/50); //incrementa velocidade com o tempo
        
         controller.Move(direction*Time.deltaTime); //mover
